4a3de818076da07e7b5bf0fdc46620737092d63c
Obilomania <iloanyaobinna@gmail.com>
2024-08-10 13:18:07 +0100

    Promo code fetch fixd

--- src/pages/admin/AdminAllPromoCodes.tsx
+++ src/pages/admin/AdminAllPromoCodes.tsx
@@ -11,7 +11,7 @@ const AdminAllPromoCodes = () => {
     });
   }, []);
 
-  let allThePromoCode = codes?.data?.result?.$values;
+  let allThePromoCode = codes?.data?.$values;
 
     const deletepromoCode = async (id:any) => {
       await adminDeletePromoCode(id);
