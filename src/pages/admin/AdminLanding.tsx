e7241063e8552fc94592d62236fea75e2ce65178
Obilomania <iloanyaobinna@gmail.com>
2024-11-14 11:02:20 +0100

    1000 screen width

--- src/pages/admin/AdminLanding.tsx
+++ src/pages/admin/AdminLanding.tsx
@@ -24,7 +24,7 @@ const AdminLanding = () => {
   useEffect(() => {
     const bounceSmallScreen = () => {
       const screenSize = window.innerWidth;
-      const computerScreenSize = 1200;
+      const computerScreenSize = 1000;
       if (screenSize < computerScreenSize) {
         toast.error("Log into a PC to access this page");
         navigate("/");
